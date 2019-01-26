4444a0878aa3504d1826ab0a548434914f480296
Kevin Yuliawan <kevin.yuliawan@gmail.com>
2019-01-26 14:25:52 -0500

    Utilize background.js to default to extension enabled: true

--- source/background.js
+++ source/background.js
@@ -1,16 +1,5 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({mtgGuildColorsEnabled: true}, function() {
-    console.log("Colors enabled:", true);
+    // console.log("Colors enabled:", true);
   });
-});
-
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-  chrome.declarativeContent.onPageChanged.addRules([{
-    conditions: [
-      // new chrome.declarativeContent.PageStateMatcher({
-      //   pageUrl: {hostEquals: 'developer.chrome.com'},
-      // })
-    ],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-  }]);
 });
